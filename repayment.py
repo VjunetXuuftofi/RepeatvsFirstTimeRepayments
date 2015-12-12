d81c023d6be632cd585d29327d84050de3f2de69
Thomas Woodside <thomas.woodside@gmail.com>
2015-12-11 18:43:44 -0800

    Commenting and adjustments.

--- repayment.py
+++ repayment.py
@@ -1,3 +1,6 @@
+# Reads through all of the Kiva API loan data snapshots and determines the repayment rate for repeat borrowers and first
+# time borrowers (identified by tags). Thanks to build.kiva.org for providing the snapshots.
+
 import json
 
 repeatpaid = 0
@@ -5,33 +8,31 @@ repeatdefault = 0
 firstpaid = 0
 firstdefault = 0
 
-for i in range(1, 1980):
+for i in range(1, 1980): #To read every loan file
     try:
         data = open('kiva_ds_json/loans/' + str(i) +'.json')
         data = json.load(data)
     except:
         continue
-    for group in data:
-        if group == "loans":
-            for loan in data[group]:
-                repeat = False
-                first = False
-                for tags in loan["tags"]:
-                    for tag in tags:
-                        if tags[tag] == "#RepeatBorrower":
-                            repeat = True
-                        if tags[tag] == "#FirstLoan":
-                            first = True
-                if repeat:
-                    if loan["status"] == "paid":
-                            repeatpaid += 1
-                    if loan["status"] == "defaulted":
-                                repeatdefault += 1
-                if first:
-                    if loan["status"] == "paid":
-                        firstpaid += 1
-                    if loan["status"] == "defaulted":
-                        firstdefault += 1
+    for loan in data["loans"]:
+            repeat = False
+            first = False
+            for tags in loan["tags"]:
+                for tag in tags:
+                    if tags[tag] == "#RepeatBorrower":
+                        repeat = True
+                    if tags[tag] == "#FirstLoan":
+                        first = True
+            if repeat:
+                if loan["status"] == "paid":
+                        repeatpaid += 1
+                if loan["status"] == "defaulted":
+                        repeatdefault += 1
+            if first:
+                if loan["status"] == "paid":
+                    firstpaid += 1
+                if loan["status"] == "defaulted":
+                    firstdefault += 1
 
-print("Repayment Rate for #RepeatBorrower: " + str((1-repeatdefault/(repeatpaid+repeatdefault))*100) + "%")
-print("Repayment Rate for #FirstLoan: " + str((1-firstdefault/(firstpaid+firstdefault))*100) + "%")
+print("Repayment Rate for #RepeatBorrower: " + str(round((1-repeatdefault/(repeatpaid+repeatdefault))*100, 2)) + "%")
+print("Repayment Rate for #FirstLoan: " + str((round((1-firstdefault/(firstpaid+firstdefault)*100), 2))) + "%")
